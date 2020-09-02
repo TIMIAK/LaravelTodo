a14b9d7eacc8d7a290ff7f70ce9502f01f1214f0
TimBaron <akiodetimothy2017@gmail.com>
2020-09-02 14:39:47 +0100

    Assessor and Mutator

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-       /* DB::insert("insert into users(name,email,password) values(?,?,?)",[
-            'Timothy','timothy@gmail.com','password'
-        ]);
-        DB::update('update users set name = "Timbaron" where id=1');
-        */
-        DB::delete('delete from users where id=1');
-        $users = DB::select('select * from users');
-        return $users;
-        //return view('home');
+        // $data = [
+        //     'name' => 'Debby',
+        //     'email'=>'Debby@gmail.com',
+        //     'password' => bcrypt('Derm'),
+        // ];
+        // //User::create($data);
+
+        // $user = User::all();
+        // return $user;
+        // // Working with Eloquent
+
+        // //Create
+        // $user = new User();
+
+        // $user->name = 'TimBaron';
+        // $user->email = 'TimBaron@gmail.com';
+        // $user->password = bcrypt('password');
+        // $user->save();
+
+        // //Read
+        // $user = User::all();
+        // return $user;
+
+        // //Delete
+        // User::where('id',2)->delete();
+
+        // //Update
+        // User::where('id',3)->update(['name' => 'Baron New Name']);
+        // $user = User::all();
+        // return $user;
+
+
+        // // Working with Raw Data
+        // //Create
+        // DB::insert("insert into users(name,email,password) values(?,?,?)",[
+        //     'Timothy','timothy@gmail.com','password'
+        // ]);
+        // //Read
+        // $users = DB::select('select * from users');
+        // return $users;
+
+        // //Update
+        // DB::update('update users set name = "Timbaron" where id=1');
+
+        // //Delete
+        // DB::delete('delete from users where id=1');
+
+
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     return view('home');
 });
 Route::get('/user','UserController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
