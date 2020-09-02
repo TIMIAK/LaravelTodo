17fd9deb4245e57a767cfb30b8f8b6326c3b82e2
TimBaron <akiodetimothy2017@gmail.com>
2020-09-02 16:36:58 +0100

    Blade Templating Engine

--- app/User.php
+++ app/User.php
@@ -27,10 +27,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    public function setPasswordAttribute($password)
-    {
-        $this->attributes['password'] = bcrypt($password);
-    }
+    // public function setPasswordAttribute($password)
+    // {
+    //     $this->attributes['password'] = bcrypt($password);
+    // }
 
     /**
      * The attributes that should be cast to native types.
